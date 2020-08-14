ff1f5ea57c74db767df85d067d2501af4f99d513
jmamadeu <jmamadeu2000@gmail.com>
2020-08-14 22:58:35 +0100

    :zap: chore: Add routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { AppLoading } from 'expo';
+import {
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from '@expo-google-fonts/amatic-sc';
+import {
+  Sniglet_400Regular,
+  Sniglet_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/sniglet';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Sniglet_400Regular,
+    AmaticSC_400Regular,
+    AmaticSC_700Bold,
+    Sniglet_800ExtraBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <StatusBar style='auto' />
+        <Routes />
+      </>
+    );
+  }
+}
